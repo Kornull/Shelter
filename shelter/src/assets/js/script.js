cbb9b5509b12252398e3dd9396055bbe75946d17
Kornull <wolod1987kor@gmail.com>
2022-11-08 18:29:50 +0300

    feat: add build

--- shelter/src/assets/js/script.js
+++
@@ -1,4 +0,0 @@
-import './burger.js';
-import './popup.js';
-import './sliderMain.js';
-import './pagination.js';
--- shelter/src/main/index.js
+++
@@ -1,4 +0,0 @@
-import './index.html';
-import './pets.html';
-import '../assets/scss/main.scss';
-import '../assets/js/script.js';
